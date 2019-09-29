d78b3b660ee86df22c13bb8bada8e1ed176d50f7
Carlos Santos <carlos.x71@hotmail.com>
2019-09-28 22:51:50 -0300

    Inicio do Layout

--- My_Report_Labor_Kivy/MyReportLabor.py
+++ My_Report_Labor_Kivy/MyReportLabor.py
@@ -1,9 +1,18 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.animation import Animation
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
+
+class Layout(BoxLayout):
+    pass
 
 class MyReportLabor(App):
     def build(self):
-        return Label(text='Hello World!')
+        return Layout()
+        #return Label(text='Hello World!')
+        #anim = Animation(x=100, y=100)
+        #anim.start(widget)
 
 MyReportLabor().run()
